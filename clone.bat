a25aa0d7e6a41fbedb93a390dbe22f4420329694
Afritz6 <svc-student@student.directory.jccc>
2017-02-01 13:21:48 -0600

    First commit

---
+++ clone.bat
@@ -0,0 +1,3 @@
+"C:\Program Files\Unity\Editor\Data\Tools\UnityYAMLMerge.exe" merge -p "Blank.unity" "Red.unity" "Blue.unity" "End.unity"
+
+"C:\Program Files\Unity\Editor\Unity.exe" -batchmode --projectPath"C:\Users\svc-student\Desktop\UnityClone\UnityExample"  -buildWindowsPlayer "C:\Users\svc-student\Desktop\build.exe"  
